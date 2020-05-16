398f9d708f253f2aed245f726a3ca4e23dceeba2
Maria Carolina Rincon Rincon <mcarolinarincon@gmail.com>
2020-05-16 21:23:16 +0200

    Agregar archivos de proyecto.

---
+++ cosmodb123/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace CosmosTableSamples
+{
+    using Microsoft.Extensions.Configuration;
+    public class AppSettings
+    {
+        public string StorageConnectionString { get; set; }
+        public static AppSettings LoadAppSettings()
+        {
+            IConfigurationRoot configRoot = new ConfigurationBuilder()
+                .AddJsonFile("Settings.json")
+                .Build();
+            AppSettings appSettings = configRoot.Get<AppSettings>();
+            return appSettings;
+        }
+    }
+}
---
+++ cosmodb123/SampleUtils.cs
@@ -0,0 +1,30 @@
+﻿public static async Task<CustomerEntity> InsertOrMergeEntityAsync(CloudTable table, CustomerEntity entity)
+{
+    if (entity == null)
+    {
+        throw new ArgumentNullException("entity");
+    }
+    try
+    {
+        // Create the InsertOrReplace table operation
+        TableOperation insertOrMergeOperation = TableOperation.InsertOrMerge(entity);
+
+        // Execute the operation.
+        TableResult result = await table.ExecuteAsync(insertOrMergeOperation);
+        CustomerEntity insertedCustomer = result.Result as CustomerEntity;
+
+        // Get the request units consumed by the current operation. RequestCharge of a TableResult is only applied to Azure Cosmos DB
+        if (result.RequestCharge.HasValue)
+        {
+            Console.WriteLine("Request Charge of InsertOrMerge Operation: " + result.RequestCharge);
+        }
+
+        return insertedCustomer;
+    }
+    catch (StorageException e)
+    {
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+        throw;
+    }
+}
